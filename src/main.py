75e8aeba773569b7aceb0e21c7010ed726e76707
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-08-11 03:02:51 +0900

    対応ルームでtsコマンドを使わなくても発言できるように変更

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ GUILD = setting.GUILD
 JIHOU = setting.JIHOU
 ROOM1 = setting.ROOM1
 help_mes = message.help_mes
-bot = commands.Bot(command_prefix="/gibara ")
+bot = commands.Bot(command_prefix="/test ")
 
 global vc
 
@@ -25,7 +25,7 @@ async def on_ready():
 
 @bot.command()
 async def find_voice(ctx):
-    channel = discord.utils.get(
+    channel = commands.utils.get(
         bot.get_all_channels(), id=ROOM1)
     print(len(channel.members))
 
@@ -45,6 +45,24 @@ async def ts(ctx, arg):
         tts.save('a.mp3')
         vc.play(discord.FFmpegPCMAudio(
             'a.mp3'))
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    await bot.process_commands(message)
+    global vc
+    try:
+        vc
+    except NameError:
+        vc = await message.author.voice.channel.connect()
+        print("ボイスチャンネルに入りま～す")
+    finally:
+        tts = gTTS(text=message.content, lang='ja')
+        tts.save('a.mp3')
+        vc.play(discord.FFmpegPCMAudio(
+            'a.mp3'))
+
 
 @bot.command()
 async def hlp(ctx):
